b6e0853255112aceee2e02f31d0372d509257b03
Elborn666 <elborn18@gmail.com>
2024-01-14 14:42:09 +0200

    зроблено модальне вікно

--- src/js/feedback-modal.js
+++ src/js/feedback-modal.js
@@ -1,11 +1,13 @@
 const body = document.querySelector('body');
 const btnOpen = document.querySelector('.js-open');
 const btnClose = document.querySelector('.js-close');
-const btnSubmit = document.querySelector('.js-submit');
+const btnSubmit = document.querySelector('.js-form');
 const modalWindow = document.querySelector('.js-modal');
 
 btnOpen.addEventListener('click', onOpenModal);
 btnClose.addEventListener('click', onCloseModal);
+btnSubmit.addEventListener('submit', onSubmitForm);
+window.addEventListener('keydown', onKeyPress);
 
 function onOpenModal() {
     modalWindow.classList.add('is-open')
@@ -15,4 +17,17 @@ function onOpenModal() {
 function onCloseModal() {
     modalWindow.classList.remove('is-open')
     body.classList.remove('hidden')
+}
+
+function onKeyPress(e) {
+    if(e.code === "Escape") {
+        onCloseModal()
+    }
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+
+    onCloseModal();
 }
