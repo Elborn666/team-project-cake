e1d798519d3f0d1c2931a451fe2efe804e5fd391
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2024-01-25 06:06:41 +0000

    Deploying to gh-pages from @ Elborn666/team-project-cake@c1459fdd525218df42ed5c26dd939df3692b249f 🚀

--- contacts.0f7943e4.js
+++ contacts.0f7943e4.js
@@ -1,2 +1,2 @@
 const e=document.querySelector("body"),t=document.querySelector(".js-open"),n=document.querySelector(".js-close"),c=document.querySelector(".js-form"),o=document.querySelector(".js-modal");function s(){o.classList.remove("is-open"),e.classList.remove("hidden")}t.addEventListener("click",(function(){o.classList.add("is-open"),e.classList.add("hidden")})),n.addEventListener("click",s),c.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset(),s()})),window.addEventListener("keydown",(function(e){"Escape"===e.code&&s()}));
-//# sourceMappingURL=card_basket.db03c5f6.js.map
+//# sourceMappingURL=contacts.0f7943e4.js.map
--- contacts.223d67e1.js
+++ contacts.223d67e1.js
@@ -1,2 +1,2 @@
 !function(){var e=document.querySelector("body"),t=document.querySelector(".js-open"),n=document.querySelector(".js-close"),c=document.querySelector(".js-form"),o=document.querySelector(".js-modal");function d(){o.classList.remove("is-open"),e.classList.remove("hidden")}t.addEventListener("click",(function(){o.classList.add("is-open"),e.classList.add("hidden")})),n.addEventListener("click",d),c.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset(),d()})),window.addEventListener("keydown",(function(e){"Escape"===e.code&&d()}))}();
-//# sourceMappingURL=card_basket.69dcf609.js.map
+//# sourceMappingURL=contacts.223d67e1.js.map
