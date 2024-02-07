1913325906c2b6fc359551161884012d558c8c93
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2024-02-07 05:49:30 +0000

    Deploying to gh-pages from @ Elborn666/team-project-cake@e8dcebf4102aeb41a830e1dda45d4a08605b9345 🚀

---
+++ contacts.710a8c86.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector(".js-modal-basket-modal"),o=document.querySelector(".js-close-basket-modal"),c=document.querySelector(".js-close-basket");function s(){e.classList.remove("hidden"),t.classList.remove("basket-is-open")}o.addEventListener("click",s),c.addEventListener("click",(function(){s()}))}();
+//# sourceMappingURL=contacts.710a8c86.js.map
---
+++ contacts.8490829d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector(".js-modal-basket-modal"),o=document.querySelector(".js-close-basket-modal"),c=document.querySelector(".js-close-basket");function s(){e.classList.remove("hidden"),t.classList.remove("basket-is-open")}o.addEventListener("click",s),c.addEventListener("click",(function(){s()}));
+//# sourceMappingURL=contacts.8490829d.js.map
