47e15320154c5339c0e0eebb827f629c3b03dc5f
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2024-02-09 06:53:57 +0000

    Deploying to gh-pages from @ Elborn666/team-project-cake@36b231a47513c2eea3cc0e98b75230e060747f03 🚀

--- index.78ce4bda.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{effect:"fade",navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3},speed:1200,loom:!0});
-//# sourceMappingURL=index.78ce4bda.js.map
