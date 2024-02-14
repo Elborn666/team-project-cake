0cc54a77472396657ffd84c4d374873bd4454a47
Elborn666 <elborn18@gmail.com>
2024-02-14 10:58:57 +0200

    зробив фільтр

--- src/js/change-value.js
+++ src/js/change-value.js
@@ -1,6 +1,23 @@
 const value = document.querySelector('.js-value');
 const changeValue = document.querySelector('.js-change-value');
+const spanTest = document.querySelector('.js-test');
+const reset = document.querySelector('.js-reset');
 
-changeValue.oninput = function() {
-    value.innerHTML = changeValue.value;
+value.addEventListener('input', onInputValue);
+changeValue.addEventListener('input', onInputRange);
+reset.addEventListener('click', onReset);
+
+function onInputRange(event) {
+    console.log(event.currentTarget.value)
+    spanTest.textContent = event.currentTarget.value
+}
+
+function onInputValue(e) {
+    console.log(e.currentTarget.value)
+    spanTest.textContent = e.currentTarget.value
+    changeValue.value = e.currentTarget.value
+}
+
+function onReset(elem) {
+    spanTest.textContent = "100"
 }
