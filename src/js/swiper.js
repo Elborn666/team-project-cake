d62c042ad618fd03b95074fe26bb6d38e576860d
Elborn666 <elborn18@gmail.com>
2024-02-19 08:45:23 +0200

    зроблено повністю секцію зі слайдером

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,16 +1,16 @@
-var swiper = new Swiper(".swiper", {
-      effect: "fade",
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-       },
-        autoplay: {
-        delay: 2500
-    },
-    speed: 1200,
+let swiper = new Swiper(".swiper", {
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500
+  },
+  speed: 1200,
   loom: true,
-    });
+});
